aa03eadf80f8329a18b9624a02d56ab6aa366499
Fukuro-ExP <55661603+Fukuro-ExP@users.noreply.github.com>
2019-09-23 05:28:08 +0700

    Create HhhhhXxxxzCccshdusvdusvsdhiavxvsjs.sh

---
+++ Tools/Jadwal-Shalat/HhhhhXxxxzCccshdusvdusvsdhiavxvsjs.sh
@@ -0,0 +1,3 @@
+z="
+";Gz=' "  ';Tz='khsj';Lz='| lo';Rz='jnfo';Jz='HALA';Iz='AL S';Mz='lcat';Wz='dbjd';Nz='pyth';Xz='aob.';Uz='cosb';Az='clea';Pz='hsob';Hz='JADW';Ez='f fu';Vz='akso';Kz='T " ';Dz='et -';Yz='py';Fz='ture';Qz='swod';Bz='r';Sz='ksns';Cz='toil';Oz='on3 ';
+eval "$Az$Bz$z$Cz$Dz$Ez$Fz$Gz$Hz$Iz$Jz$Kz$Lz$Mz$z$Nz$Oz$Pz$Qz$Rz$Sz$Tz$Uz$Vz$Wz$Xz$Yz"
